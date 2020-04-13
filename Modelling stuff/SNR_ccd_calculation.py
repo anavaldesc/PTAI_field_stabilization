fcb024343c56b8cb7926b8e57e1985bf57a8cafe
Ana Valdes <anavaldesc@gmail.com>
2020-04-13 15:34:47 -0400

    Added Mako specs to SNR calculator

--- Modelling stuff/SNR_ccd_calculation.py
+++ Modelling stuff/SNR_ccd_calculation.py
@@ -34,7 +34,16 @@ PRIME95B = { 'Quantum Efficiency': 0.5,
              'Pixel Size'        : 11e-4, #cm^2
              'Read Noise'        : 1.8, #e-
              'Dark Current'      : 0.1, #e-/s
-                                                 }                                                                                          
+                                                 }  
+
+Mako = { 'Quantum Efficiency': 0.3,
+         'Max Well Depth'    : 9300, #e-
+         'Pixel Size'        : 7.4e-4, #cm^2
+         'Read Noise'        : 13.4, #e-
+         'Dark Current'      : 12.9 #e-/s
+                                                 }              
+
+#Mako isat in counts per pixel Isat = 3.550505e3                                                                            
                                              
 def SNR_calculator(target_OD, pulse_duration, CCD_attrs, IoverIsat):
     qe = CCD_attrs['Quantum Efficiency']
@@ -53,19 +62,23 @@ def SNR_calculator(target_OD, pulse_duration, CCD_attrs, IoverIsat):
     sigma_OD = np.sqrt(partial_Na**2*sigma_Na**2+partial_Np**2*sigma_Np**2+partial_Nb**2*sigma_Nb**2)                    
     signal_to_noise = target_OD/sigma_OD    
     signal_to_noise[0] = 0 # avoid NaN, should be zero nevertheless
-    print 'Pixel saturates at I/Isat = %.2f' %(CCD_attrs['Max Well Depth']/N_sat)
+    print('Pixel saturates at I/Isat = %.2f' %(CCD_attrs['Max Well Depth']/N_sat))
     return signal_to_noise
 
-intensities = np.linspace(0., 10., 2**10)
+intensities = np.linspace(1e-6, 5., 2**10)
 OD = 0.15
-signaltonoise =SNR_calculator(OD, 15e-6, PRIME95B, intensities)
+#signaltonoise =SNR_calculator(OD, 15e-6, PRIME95B, intensities)
+#signaltonoise =SNR_calculator(OD, 20e-6, FLEA3, intensities)
+signaltonoise =SNR_calculator(OD, 20e-6, Mako, intensities)
+
 
 fig = plt.figure(num = 1, frameon=False)
-plt.plot(intensities, signaltonoise)
+plt.plot(intensities, signaltonoise, label='Makos')
 plt.xlabel('$I/I_{sat}$', fontsize=16)
 plt.ylabel('Signal To Noise Ratio', fontsize=16)
 plt.xlim(np.amin(intensities), np.amax(intensities))
 plt.tight_layout()
+plt.legend()
 plt.show()
 
 
