9861e9e7ca6712baf869c36dacdc816512afcedf
Ana Valdes <anavaldesc@gmail.com>
2020-04-16 15:02:25 -0400

    Multiple changes
    -Added figure for uwave pulses
    -Added paco's thesis chapter
    -Updated the SNR code to latest version from Paco
    -Some structuring of manuscript

--- Modelling stuff/SNR_ccd_calculation.py
+++ Modelling stuff/SNR_ccd_calculation.py
@@ -5,81 +5,193 @@ Created on Tue Jan 17 13:46:02 2017
 @author: Francisco Salces 
 """
 
-from __future__ import division
-import numpy as np
-from matplotlib import pyplot as plt
+#from __future__ import division
+#import numpy as np
+#from matplotlib import pyplot as plt
+#
+## Constants
+#planck = 6.626e-34 # Plancks constant
+#Isat = 1.67e-3     # Rb87
+#magnification = 5.33
+#frequency = 3e8/780.24e-9
+#
+#FLEA3 =    { 'Quantum Efficiency': 0.3,
+#             'Max Well Depth'    : 23035.06, #e-
+#             'Pixel Size'        : 5.6e-4, #cm^2
+#             'Read Noise'        : 38.74, #e-
+#             'Dark Current'      : 307.92, #e-/s
+#                                                 }
+#                                             
+#ZYLA_5p5 = { 'Quantum Efficiency': 0.35,
+#             'Max Well Depth'    : 30000, #e-
+#             'Pixel Size'        : 6.5e-4, #cm^2
+#             'Read Noise'        : 2.7, #e-
+#             'Dark Current'      : 0.01, #e-/s
+#                                                 }
+#                                             
+#PRIME95B = { 'Quantum Efficiency': 0.5,
+#             'Max Well Depth'    : 80000, #e-
+#             'Pixel Size'        : 11e-4, #cm^2
+#             'Read Noise'        : 1.8, #e-
+#             'Dark Current'      : 0.1, #e-/s
+#                                                 }  
 
-# Constants
-planck = 6.626e-34 # Plancks constant
-Isat = 1.67e-3     # Rb87
-magnification = 5.33
-frequency = 3e8/780.24e-9
-
-FLEA3 =    { 'Quantum Efficiency': 0.3,
-             'Max Well Depth'    : 23035.06, #e-
-             'Pixel Size'        : 5.6e-4, #cm^2
-             'Read Noise'        : 38.74, #e-
-             'Dark Current'      : 307.92, #e-/s
-                                                 }
-                                             
-ZYLA_5p5 = { 'Quantum Efficiency': 0.35,
-             'Max Well Depth'    : 30000, #e-
-             'Pixel Size'        : 6.5e-4, #cm^2
-             'Read Noise'        : 2.7, #e-
-             'Dark Current'      : 0.01, #e-/s
-                                                 }
-                                             
-PRIME95B = { 'Quantum Efficiency': 0.5,
-             'Max Well Depth'    : 80000, #e-
-             'Pixel Size'        : 11e-4, #cm^2
-             'Read Noise'        : 1.8, #e-
-             'Dark Current'      : 0.1, #e-/s
-                                                 }  
-
-Mako = { 'Quantum Efficiency': 0.3,
-         'Max Well Depth'    : 9300, #e-
-         'Pixel Size'        : 7.4e-4, #cm^2
-         'Read Noise'        : 13.4, #e-
-         'Dark Current'      : 12.9 #e-/s
-                                                 }              
+            
 
 #Mako isat in counts per pixel Isat = 3.550505e3                                                                            
                                              
-def SNR_calculator(target_OD, pulse_duration, CCD_attrs, IoverIsat):
-    qe = CCD_attrs['Quantum Efficiency']
-    pixel_area = (CCD_attrs['Pixel Size']/magnification)**2
-    single_photon_energy = planck*frequency
-    N_sat = (qe*pixel_area*pulse_duration*Isat)/(single_photon_energy)
-    N_probe = N_sat*IoverIsat
-    N_atoms = N_probe*np.exp(-target_OD)
-    N_back = 0.0
-    sigma_Na = np.sqrt(N_atoms)
-    sigma_Np = np.sqrt(N_probe)
-    sigma_Nb = np.sqrt(CCD_attrs['Read Noise']**2 + (CCD_attrs['Dark Current']*pulse_duration)**2)    
-    partial_Na = -1/N_sat - 1/(N_atoms-N_back)
-    partial_Np = 1/N_sat + 1/(N_probe-N_back)
-    partial_Nb = (N_probe-N_atoms)/((N_atoms-N_back)*(N_probe-N_back))   
-    sigma_OD = np.sqrt(partial_Na**2*sigma_Na**2+partial_Np**2*sigma_Np**2+partial_Nb**2*sigma_Nb**2)                    
-    signal_to_noise = target_OD/sigma_OD    
-    signal_to_noise[0] = 0 # avoid NaN, should be zero nevertheless
-    print('Pixel saturates at I/Isat = %.2f' %(CCD_attrs['Max Well Depth']/N_sat))
-    return signal_to_noise
+import numpy as np
+np.warnings.filterwarnings('ignore')
+from matplotlib import rc
+rc('font', family='serif')
+rc('font', size=18)
+rc('text', usetex=True)
+import matplotlib.pyplot as plt
 
-intensities = np.linspace(1e-6, 5., 2**10)
-OD = 0.15
-#signaltonoise =SNR_calculator(OD, 15e-6, PRIME95B, intensities)
-#signaltonoise =SNR_calculator(OD, 20e-6, FLEA3, intensities)
-signaltonoise =SNR_calculator(OD, 20e-6, Mako, intensities)
+# SI units everywhere
+Hz, kHz, MHz, GHz = 1., 1e3, 1e6, 1e9
+s, ms, us, ns = 1., 1e-3, 1e-6, 1e-9
+m, cm, mm, um, nm = 1., 1e-2, 1e-3, 1e-6, 1e-9
+W, mW, uW = 1., 1e-3, 1e-6
+pe = 1. # photo electrons
 
+from scipy.constants import codata
+hbar = codata.value('Planck constant over 2 pi')
+c = codata.value('speed of light in vacuum')
+pi = codata.pi
 
-fig = plt.figure(num = 1, frameon=False)
-plt.plot(intensities, signaltonoise, label='Makos')
-plt.xlabel('$I/I_{sat}$', fontsize=16)
-plt.ylabel('Signal To Noise Ratio', fontsize=16)
-plt.xlim(np.amin(intensities), np.amax(intensities))
-plt.tight_layout()
-plt.legend()
-plt.show()
+DEBUG = True                                                                                        
 
+def OD_opt_funct(x, s, y):
+    x = np.abs(x) # Always positive
+    return np.log(x) - np.log(s) - (s)*(1-x) + y
 
+def SNR_calculator(target_OD, pulse_duration, CCD, s0, technical_noise=True):
+    """ Calculates the signal-to-noise ratio for the optical depth (OD) as a
+    function of the intensity in units of Isat. As input, it uses the target
+    OD, the duration of the resonant probe pulse in seconds, a CCD dictionary
+    containing the sensor specs, the saturation parameter s0=I/Isat, and a 
+    boolean to include technical noise contributions in the calculation. """
+    
+    QE = CCD['Quantum Efficiency']
+    eff_pix_size = CCD['Pixel Size']
+    eff_pixel_area = (eff_pix_size / magnification) ** 2
+    single_photon_energy = hbar * 2 * pi * frequency
+    
+    # Nx denotes numbers of photoelectrons.
+    # From here on, assume photo(electro)n shot noise for any measured Nx
+    N_sat = (QE * eff_pixel_area * pulse_duration * Isat) / (single_photon_energy)
+    N_probe = N_sat * s0
+    if technical_noise:
+        N_read = CCD['Read Noise']
+        N_dark = CCD['Dark Current'] * pulse_duration
+    else:
+        N_read, N_dark = 0.0, 0.0
+    if DEBUG:
+        print(fR"Computing SNR for a target OD of {target_OD:.2f}")
+        print(fR'The saturation intensity corresponds to {N_sat:.1f} p.e.')
+        print(fR"Note that this detector saturates at {CCD['Max Well Depth']:.1f} p.e., or {CCD['Max Well Depth']/N_sat:.2f} Isat")
+    
+    # For the number of detected absorbed photoelectrons, solve the
+    # transcendental equation ln(x/s) - s*(1-x) + y = 0
+    # where x = Na/Nsat, s = Np/Nsat, and y = OD. This avoids
+    # any s << 1 assumptions.
+    from scipy.optimize import minimize
+    
+    N_atoms = []
+    for s in N_probe / N_sat:
+        N_atoms_solver = minimize(OD_opt_funct, x0=10, args=(s, target_OD))
+        N_atoms.append(np.abs(N_atoms_solver.x[0]) * N_sat)
+    
+    N_atoms = np.array(N_atoms)
+    if DEBUG:
+        # Compare with naive Beer's law (unsaturated)
+        N_beer = N_probe * np.exp(-target_OD)  # Beer's law
+        print(
+            fR"The integrated absorbed number is {N_atoms.sum():.1f} p.e., "
+            + fR"or {N_atoms.sum()/N_beer.sum():.2f} the number from Beer's law"
+        )
+
+    # Shot noise contributions
+    sigma_Na = np.sqrt(N_atoms + N_read**2 + N_dark**2)
+    sigma_Np = np.sqrt(N_probe + N_read**2 + N_dark**2)
+
+    # Analytic uncorrelated error propagation
+    sqpartial_Na = (1 + N_atoms / N_sat) ** 2 / (N_atoms ** 2)
+    sqpartial_Np = (1 + N_probe / N_sat) ** 2 / (N_probe ** 2)
+    
+    sigma_OD = np.sqrt(sqpartial_Na * sigma_Na ** 2 + sqpartial_Np * sigma_Np ** 2)
+    
+    # Estimated SNR
+    signal_to_noise = target_OD / sigma_OD
+    if DEBUG:
+        print(
+            fR"The peak SNR is {signal_to_noise.max():.2f}, at ~ {s0[signal_to_noise.argmax()]:.2f} Isat"
+        )
+
+    # "Chop" SNR from sensor saturation point onward
+    saturation_mask = np.zeros_like(s0)
+    saturation_mask = s0 < CCD['Max Well Depth']/N_sat
+    return signal_to_noise * saturation_mask
+
+if __name__ in '__main__':
+
+    # Imaging characteristics
+    Isat = 1.67 * mW / (cm ** 2)
+    magnification = 6.0
+    wavelength = 780.24 * nm
+    wavenumber = 2 * pi / wavelength
+    frequency = c / wavelength
+    sigma_0 = (3 * wavelength ** 2) / (2 * np.pi)
+
+    # Sensor specs
+    FLEA3 = {
+        'Quantum Efficiency': 0.3,
+        'Max Well Depth': 23035.06 * pe,
+        'Pixel Size': 5.6 * um,
+        'Read Noise': 38.74 * pe,
+        'Dark Current': 307.92 * pe / s,
+    }
+                                                 
+    ZYLA_5p5 = {
+        'Quantum Efficiency': 0.35,
+        'Max Well Depth': 30000 * pe,
+        'Pixel Size': 6.5 * um,
+        'Read Noise': 2.7 * pe,
+        'Dark Current': 0.01 * pe / s,
+    }
+                                                 
+    PRIME95B = {
+        'Quantum Efficiency': 0.5,
+        'Max Well Depth': 80000 * pe,
+        'Pixel Size': 11 * um,
+        'Read Noise': 1.8 * pe,
+        'Dark Current': 0.1 * pe / s,
+    }
+
+    Mako = { 'Quantum Efficiency': 0.3,
+             'Max Well Depth'    : 9300 * pe, #e-
+             'Pixel Size'        : 7.4 * um, #cm^2
+             'Read Noise'        : 20 * pe, #e-
+             'Dark Current'      : 120 * pe / s#e-/s
+    }  
+    # Avoid using zero intensity to avoid numerical overflow
+    intensities = np.logspace(-2, 2, 2**10)
+    target_linear_density = 10/um
+    pulse_length = 20*us
+    CCD = Mako
+
+    # Optical depth at the object plane
+    OD = 0.05#target_linear_density * sigma_0 / CCD['Pixel Size']
+    signaltonoise = SNR_calculator(OD, pulse_length, CCD, intensities)
+    fig = plt.figure(num=1)
+    plt.plot(
+        intensities[signaltonoise > 0], signaltonoise[signaltonoise > 0], c='crimson'
+    )
+    plt.xlabel(r'$I/I_{sat}$', fontsize=16)
+    plt.ylabel(r'OD Signal To Noise Ratio', fontsize=14)
+    plt.xscale('log')
+    plt.xlim(np.amin(intensities), np.amax(intensities))
+    plt.tight_layout()
+    plt.show()
 
